ad3978df24f6845d19b9df7b2c3f6f229444aab5
NoaLeGeek68 <noacraft68@gmail.com>
2022-03-27 15:51:56 +0200

    fillColumn InventoryUtils

--- src/main/java/net/sunderia/skyblock/utils/InventoryUtils.java
+++ src/main/java/net/sunderia/skyblock/utils/InventoryUtils.java
@@ -5,7 +5,7 @@ import org.bukkit.inventory.ItemStack;
 
 public class InventoryUtils {
 
-    public Inventory fillAll(Inventory inventory, ItemStack itemStack, int rows){
+    public Inventory fill(Inventory inventory, ItemStack itemStack, int rows){
         for(int index = 0; index < rows * 9; index++){
             inventory.setItem(index, itemStack);
         }
@@ -14,6 +14,13 @@ public class InventoryUtils {
 
     public Inventory fillRow(Inventory inventory, ItemStack itemStack, int row){
         for(int index = row * 9 - 9; index < row * 9; index++){
+            inventory.setItem(index, itemStack);
+        }
+        return inventory;
+    }
+
+    public Inventory fillColumn(Inventory inventory, ItemStack itemStack, int column, int rows){
+        for(int index = column - 1; index < column + (rows * 9); index += 9){
             inventory.setItem(index, itemStack);
         }
         return inventory;
